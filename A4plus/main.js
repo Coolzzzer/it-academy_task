c0a362fcf9d5917bff7a6f53e7ade529c5dfb2f8
Coolzzzer <157237733+Coolzzzer@users.noreply.github.com>
2024-05-16 12:09:26 +0300

    Add files via upload

---
+++ A4plus/main.js
@@ -0,0 +1,37 @@
+"use strick"
+let string = prompt("");
+let newStr = "";
+
+function removingUnnecessary(str){
+	str = str.toLowerCase();
+	for(let i = 0;i < str.length;i++){
+		if(str[i] == " " || str[i] == "СЊ" || str[i] == "СЉ" 
+		|| str[i] == "," || str[i] == "." || str[i] == "!" 
+		|| str[i] == "?" || str[i] == "%" || str[i] == "в„–" 
+		|| str[i] == ":" || str[i] == ";" || str[i] == "-"){
+		} else{
+			newStr += str[i]
+		}
+	}
+	return newStr
+}
+removingUnnecessary(string);
+
+let start = 0;
+let end = newStr.length - 1;
+
+function isPalindrome(newStr){
+	if(start>=end){
+		return true
+	}else{
+		if(newStr[start] === newStr[end]){
+			start++;
+			end--;
+			return (isPalindrome(newStr)
+			)
+		}else{
+			return false
+		}
+	}
+}
+alert(isPalindrome(newStr)?"это палиндром":"это не палиндром")
