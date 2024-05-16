0b906c77eed577c25982c54a03c23f675626dd04
Coolzzzer <157237733+Coolzzzer@users.noreply.github.com>
2024-05-16 17:35:11 +0300

    Add files via upload

--- A4plus/main.js
+++ A4plus/main.js
@@ -1,37 +1,35 @@
 "use strick"
 let string = prompt("");
-let newStr = "";
 
-function removingUnnecessary(str){
+function isPalindrome(str){
+	let newStr = "";
 	str = str.toLowerCase();
 	for(let i = 0;i < str.length;i++){
-		if(str[i] == " " || str[i] == "ь" || str[i] == "ъ" 
-		|| str[i] == "," || str[i] == "." || str[i] == "!" 
-		|| str[i] == "?" || str[i] == "%" || str[i] == "№" 
-		|| str[i] == ":" || str[i] == ";" || str[i] == "-"){
-		} else{
-			newStr += str[i]
+		if(str[i] !== " " && str[i] !== "ь" && str[i] !== "ъ" 
+		&& str[i] !== "," && str[i] !== "." && str[i] !== "!" 
+		&& str[i] !== "?" && str[i] !== "%" && str[i] !== "!№" 
+		&& str[i] !== ":" && str[i] !== ";" && str[i] !== "-")
+		{
+		newStr += str[i]
 		}
 	}
-	return newStr
-}
-removingUnnecessary(string);
-
-let start = 0;
-let end = newStr.length - 1;
-
-function isPalindrome(newStr){
-	if(start>=end){
-		return true
-	}else{
-		if(newStr[start] === newStr[end]){
-			start++;
-			end--;
-			return (isPalindrome(newStr)
-			)
+	let start = 0;
+	let end = newStr.length - 1;
+	function comparison(newStr){
+		if(start>=end){
+			return true
 		}else{
-			return false
+			if(newStr[start] === newStr[end]){
+				start++;
+				end--;
+				return (comparison(newStr)
+				)
+			}else{
+				return false
+			}
 		}
 	}
+	return comparison(newStr)
 }
-alert(isPalindrome(newStr)?"это палиндром":"это не палиндром")
+
+alert(isPalindrome(string)?"это палиндром":"это не палиндром")
