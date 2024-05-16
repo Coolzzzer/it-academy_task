92f99e238a462a3d12c21de774d9dd16745445ce
Coolzzzer <157237733+Coolzzzer@users.noreply.github.com>
2024-05-16 20:36:51 +0300

    Add files via upload

--- A4plus/main.js
+++ A4plus/main.js
@@ -5,31 +5,29 @@ function isPalindrome(str){
 	let newStr = "";
 	str = str.toLowerCase();
 	for(let i = 0;i < str.length;i++){
-		if(str[i] !== " " && str[i] !== "ь" && str[i] !== "ъ" 
+		if(str[i] == "ё"){
+			return newStr += "e";
+		}else if(
+			 str[i] !== " " && str[i] !== "ь" && str[i] !== "ъ" 
 		&& str[i] !== "," && str[i] !== "." && str[i] !== "!" 
-		&& str[i] !== "?" && str[i] !== "%" && str[i] !== "!№" 
-		&& str[i] !== ":" && str[i] !== ";" && str[i] !== "-")
-		{
+		&& str[i] !== "?" && str[i] !== "%" && str[i] !== "№" 
+		&& str[i] !== ":" && str[i] !== ";" && str[i] !== "-"){
 		newStr += str[i]
-		}
+		} 
 	}
-	let start = 0;
-	let end = newStr.length - 1;
-	function comparison(newStr){
+	function comparison(newStr, start, end){
 		if(start>=end){
 			return true
 		}else{
 			if(newStr[start] === newStr[end]){
-				start++;
-				end--;
-				return (comparison(newStr)
+				return (comparison(newStr, start++,end--)
 				)
 			}else{
 				return false
 			}
 		}
 	}
-	return comparison(newStr)
+	return comparison(newStr, 0, newStr.length - 1)
 }
 
 alert(isPalindrome(string)?"это палиндром":"это не палиндром")
