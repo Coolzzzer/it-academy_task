f660b912ee2929df22153825a3032d54fb11ab27
Coolzzzer <157237733+Coolzzzer@users.noreply.github.com>
2024-05-22 21:46:52 +0300

    Add files via upload

---
+++ B6plus/main.js
@@ -0,0 +1,34 @@
+function buildWrapper(tagName) {
+  function tagBuild(cont, attribute = "") {
+		let string = "";
+    let attributeString = Object.entries(attribute).map(([key, value]) => ` ${key}:'${(key==="title") ? symbolCheck(value, string): value}'`);
+		function symbolCheck(content, mnemonicsString){
+			toString(content)
+			for (let i=0;i<content.length;i++){
+				if(content[i] === "<"){
+					mnemonicsString += "&lt;"
+				}else if(content[i] === ">"){
+					mnemonicsString += "&gt;"
+				}else if(content[i] === '"'){
+					mnemonicsString += "&quot;"
+				}else if(content[i] === "'"){
+					mnemonicsString += "&apos;"
+				}else if(content[i] === "&"){
+					mnemonicsString += "&apos;"
+				}else{
+					mnemonicsString += content[i]
+				}
+			}
+			return mnemonicsString
+		}
+    return `<${tagName}${attributeString}>${symbolCheck(cont, string)}</${tagName}>`;
+  }
+  return tagBuild;
+}
+
+var wrapP = buildWrapper("P");
+console.log(wrapP("Однажды в студёную зимнюю пору"));
+console.log(wrapP("Однажды в студёную зимнюю пору", { lang: "ru" }));
+console.log( wrapP("Однажды в <студёную> зимнюю пору") );
+var wrapH1=buildWrapper("H1");
+console.log( wrapH1("СТИХИ",{align:"center",title:"M&M's"}) );
